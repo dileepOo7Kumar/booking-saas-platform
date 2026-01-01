dd6cdada9e0b1f9cdf2996431e7262f8dbb7e8cf
Dileep Kumar <kumar7.dileep@gmail.com>
2026-01-01 23:18:44 +0530

    chore(auth-service): initialize Spring Boot service

---
+++ auth-service/src/test/java/com/bookingsaas/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bookingsaas.authservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Disabled until datasource is configured")
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
